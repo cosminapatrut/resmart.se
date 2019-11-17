9652cc239e5c120c2bdf9a455e47ce2f15193c6f
cosminapatrut <cosminamadalina.patrut@gmail.com>
2019-11-17 20:51:25 +0200

    Final version

--- src/js/milestones/Milestone5.jsx
+++ src/js/milestones/Milestone5.jsx
@@ -3,11 +3,11 @@ import React, { Component } from "react";
 import "../../css/Milestones.css";
 import facebookLogo from "../../res/facebook.jpg";
 import linkedinLogo from "../../res/linkedin.jpg";
-import fb1 from "../../res/PostFbEgov.jpg"
-import fb2 from "../../res/PostFbMAster.jpg"
+import fb1 from "../../res/postEgov.JPG"
+import fb2 from "../../res/postMaster.JPG"
 import linkedinpost from "../../res/PostLinkedin.jpg"
-import analytics from "../../res/analytics.JPG"
-import hotjar from "../../res/hotjar.JPG"
+import analytics from "../../res/postanalytics.JPG"
+import hotjar from "../../res/posthotjar.JPG"
 
 export default class Milestone5 extends Component {
   render() {
@@ -51,10 +51,10 @@ export default class Milestone5 extends Component {
         </p>
         <p className="w3-text-grey">
           The first one is one of the biggest master students related group on
-          Facebook.
+          Facebook. In this group we have reached 34 views.
         </p>
         <p className="w3-text-grey">
-          The second one contains all out colleagues.
+          The second one contains all our colleagues.
         </p>
         <div className="screenshots-container"
           style={{
@@ -69,8 +69,8 @@ export default class Milestone5 extends Component {
         </div>
 
         <p className="w3-text-grey">
-          On <b>LinkedIn</b>, one of us posted on his feed and he
-          reached TODO views.
+          On <b>LinkedIn</b>, one of us posted on his feed and we
+          reached 67 views.
         </p>
 
         <div
@@ -103,18 +103,17 @@ export default class Milestone5 extends Component {
           <img style={{height: "300px"}} src={analytics} alt=""/>
         </div>
         <p className="w3-text-grey">
-          According to Google Analytics we have 28 different users that visited
-          our page and according to Hotjar 32. We highlighted the most important
+          According to Google Analytics we have 31 different users that visited
+          our page and according to Hotjar 16 from desktop devices and 47 from mobile devices. We highlighted the most important
           sources of our traffic in the figure above and as you can see, most of
           our traffic comes from direct sources. Because how LinkedIn works the
           traffic that came from this social media website is included in direct
           traffic. So, the social media channel that worked best for us is
-          LinkedIn with 47,2% of our traffic. This result was expected because
+          LinkedIn. This result was expected because
           on LinkedIn we have a lot of people interested in our field. After
-          that, on the second place is our course page with 22,2%. The reason is
+          that, on the second place is hotjar which registered 63 views. The reason is
           the same as above, but the audience wasn’t as much as on LinkedIn. On
-          the thirth place we have Facebook with an accumulated 19,5% (Facebook
-          website + Facebook messenger + mobile app). Even if the reach was the
+          the thirth place we have Facebook. Even if the reach was the
           greatest in this case, the results weren’t as good as we expected.
           Maybe this happened because people don’t pay as much attention of
           Facebook as they used to or maybe we haven’t chose the audience very
@@ -134,14 +133,14 @@ export default class Milestone5 extends Component {
         <p className="w3-text-grey">
           A nice feature that Hotjar provide are heatmaps. Using this feature we
           can see where people spent most of their time on our page and where
-          they clicked. Analyzing this data, we observed that 87 clicks were
-          recorded and the most of the clicks (22,99%) were on “About”
+          they clicked. Analyzing this data, we observed that 63 clicks were
+          recorded and the most of the clicks were on “About”
           option. This means that people are interested in our application and
           they can’t wait to get started.
         </p>
 
         <p className="w3-text-grey">
-          So far we have accomplished to convince TODO people to subscribe to
+          So far we have accomplished to convince 7 people to subscribe to
           our newsletters.
         </p>
       </div>
